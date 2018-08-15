385dde220728672e857f8510b804cb7d8fa053e6
dineysd <dineysd@hotmail.com>
2018-08-15 15:02:21 -0300

    projeto criado

---
+++ src/main/java/com/dineydev/cursoVendas/CursoVendasApplication.java
@@ -0,0 +1,12 @@
+package com.dineydev.cursoVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoVendasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dineydev/cursoVendas/CursoVendasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dineydev.cursoVendas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoVendasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
